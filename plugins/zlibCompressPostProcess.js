2dbe197bda770a26beddee7f53a25e1a26c1c718
Tim van Scherpenzeel <timvanscherpenzeel@Tims-MacBook-Pro.local>
2018-07-23 16:25:29 +0200

    added zlib compression and decompression

---
+++ plugins/zlibCompressPostProcess.js
@@ -0,0 +1,13 @@
+// Vendor
+const pako = require('pako');
+
+module.exports = (result) => {
+	const data = result;
+	const { glb } = data;
+	const compressedGlb = pako.deflate(glb);
+
+	return {
+		glb: compressedGlb,
+		separateResources: data.separateResources,
+	};
+};
